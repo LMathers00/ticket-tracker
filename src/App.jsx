8027b662f0a93a231aab9e9c28ee26992c096763
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-20 16:11:51 +0100

    Files all set out with code in for jsx although currently doesn't display what i've built

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// Need to create folders and files to display :
+// The overall box
+// Name and occupation 
+//COunter and display the number 
+//Then maybe add some special effects instead of plain and boring 
+// MUST HAVE SOME FORM OF DESIGN 
+// But focus on functionality first 
+
+import "./App.css";
+import TicketCounter from './components/TicketCounter/TicketCounter.jsx';
+
+import team from './data/team-data';
+
+const ticketCardArray = team.map((employee) => {
+  return (
+    <section className="counter-container">
+      <TicketCounter name={employee.name} role={employee.role} key = {employee.id}/>
+    </section>
+  );
+})
+
+const App = () => {
+  return (
+    <div className="app">
+      <main className="app-container">
+        <h1 className="app-container__title">Ticket Tracker</h1>
+        <section className="app-container__ticket-container">
+          {ticketCardArray}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
